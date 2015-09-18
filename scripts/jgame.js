1761359b6a92f25481b06b0cabe4b672990ab924
jahmal newton <jnewton1@ggc.edu>
2015-09-18 14:10:20 -0400

    creation of the game class and upload the standard vairables

---
+++ scripts/jgame.js
@@ -0,0 +1,29 @@
+// @author Jahmal Newton
+// Class: jgame.js
+// Version 1.0 ITEC 4860 Fall 2015
+// Written September 9, 2015
+
+
+//Assigns varibles for Unhinged
+gonetoforest = false;
+gonetocastle = false;
+gonetovillage = false;
+beenhungry = false;
+earnabonus = false;
+doorunlocked = false;
+exitunlocked = false;
+beentoexit = false;
+
+//List possible inventory items
+twigs = false;
+apple = false;
+shield = false;
+stone = false;
+weathereddagger = false;
+shirt = false;
+tunic = false;
+lightarmor = false;
+slingshot = false;
+key = false;
+powder = false;
+
