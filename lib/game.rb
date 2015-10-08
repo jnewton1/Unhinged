a52ca6afb7a74d0d9d8f0da74def20486371efe8
Jahmal Newton <jnewton1@ggc.edu>
2015-10-08 22:04:05 +0000

    update game module

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,15 @@
 module Game
     castle = Level.new("A scary castle")
     village = Level.new("A scary village")
+    forest = Level.new("a gloomy forest")
     desert = Level.new("A scary desert")
     artic = Level.new("A frozen wasteland")
+    
 
     castle.direction ({:north => village})
-    village.direction({:south => castle, :west => desert})
-    desert.direction()
+    village.direction({:south => castle, :east => desert, :west => forest})
+    desert.direction({:north => artic, :west => village})
+    forest.direction ({:east => village})
+    artic.direction ({:south => desert})
+
 end
