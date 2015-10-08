586d1587be913e2abafcd51112cb924862b72237
Jahmal Newton <jnewton1@ggc.edu>
2015-10-08 21:59:02 +0000

    update gam module

--- lib/game.rb
+++ lib/game.rb
@@ -2,5 +2,9 @@ module Game
     castle = Level.new("A scary castle")
     village = Level.new("A scary village")
     desert = Level.new("A scary desert")
+    artic = Level.new("A frozen wasteland")
 
+    castle.direction ({:north => village})
+    village.direction({:south => castle, :west => desert})
+    desert.direction()
 end
