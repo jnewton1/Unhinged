bb01c22996c0e15e4bd3a1b236326dd459ab5740
Jahmal Newton <jnewton1@ggc.edu>
2015-10-27 22:05:53 +0000

    update the code

--- app.rb
+++ app.rb
@@ -4,6 +4,17 @@ require './lib/game.rb'
 set :public_folder, "public"
 
 get '/' do
-  level= Game::castle
-  erb :index, :locals => { :desc => level.description }
+  @story="You are in a dark forest. It's cold and raining."
+  @followup = "To the north there is a cave (1) and to the east there is a tree (2), choose your destiny"
+  #level= Game::castle
+  erb :index
 end
+
+post '/console' do
+  if params['command'] == '1'
+    erb :cave
+  else 
+    erb :tree
+  end
+end
+
--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,8 @@
 require './lib/level.rb'
 module Game 
-    castle = Level.new("A scary castle")
-    village = Level.new("A scary village")
-    forest = Level.new("a gloomy forest")
-    desert = Level.new("A scary desert")
-    artic = Level.new("A frozen wasteland")
-    
-
-    castle.direction ={:north => village}
-    village.direction ={:south => castle, :east => desert, :west => forest}
-    desert.direction ={:north => artic, :west => village}
-    forest.direction ={:east => village}
-    artic.direction ={:south => desert}
-
+#    castle = Level.new("A scary castle")
+#    village = Level.new("A scary village")
+ #   forest = Level.new("a gloomy forest")
+  #  desert = Level.new("A scary desert")
+   # artic = Level.new("A frozen wasteland")
 end
