dfa3e88aa52b725a30fe2bbdfdb40231c96dd41b
Mervyn Walton <mwalton1@ggc.edu>
2015-11-05 23:44:27 +0000

    added gameplay dragon attack

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require './lib/game.rb'
 set :public_folder, "public"
 
 get '/' do
+  @goldcount = 0
   @story="You are in a dark forest. It's cold and raining."
   @followup = "To the north there is a cave (1) and to the east there is a tree (2), choose your destiny"
   #level= Game::castle
@@ -20,4 +21,25 @@ end
 
 post '/startfire' do
   erb :startfire
+end
+
+
+post '/processgold' do
+  if params['goldcount'].to_i > 50
+    erb :dragonattack
+  else
+    #store inventory
+    @goldcount = params['goldcount'].to_i
+    erb :tree
+    #return "#{params['goldcount'].to_i} added to gold inventory"
+ # return params['goldcount']
+  end
+end
+
+post '/movement' do
+  @status = "continue"
+  if params['movement'].eql?"UP"
+    return erb :cave
+  else
+  end
 end
